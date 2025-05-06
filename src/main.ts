c4f6af61aaff4952cad57d5e5fecd37964d957da
Doniyor Qalandarov <doniyorqalandarov853@gmail.com>
2025-05-06 17:18:57 +0500

    add

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('OnlineShop example')
+    .setDescription('The OnlineShop API description')
+    .setVersion('1.0')
+    .addTag('OnlineShop ')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const port = process.env.PORT || 4445;
   await app.listen(port, () => {
-    console.log(`server running is ${port}`);
+    console.log(`server running is http://localhost:${port}`);
   });
 }
 bootstrap();
